ad2e6c0ceceeede455f3d1ec267f0e1d5977bc8b
Pavel Molchanov <elsy1999@yandex.ru>
2024-01-09 09:23:56 +0300

    Add repo

---
+++ src/main/java/com/example/yoyoshop/YoyoshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoyoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoyoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoyoshopApplication.class, args);
+	}
+
+}
